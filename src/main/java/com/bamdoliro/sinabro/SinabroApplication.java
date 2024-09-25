9e6da61ff22fccf1925d79045219bd71ea43ed97
jyj1289 <jin37091289@gmail.com>
2024-09-25 16:31:20 +0900

    Initialize

---
+++ src/main/java/com/bamdoliro/sinabro/SinabroApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.sinabro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinabroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinabroApplication.class, args);
+    }
+
+}
