30cf7e2b1cf5f472d11d26163501e22c598377dc
cabbage16 <qocn16@gmail.com>
2024-11-13 02:19:32 +0900

    feat(#1): UserRepository 추가 - UserRepository 를 추가했어요 - JpaAuditing 활성화 했어요.

--- src/main/java/com/bamdoliro/sinabro/infrastructure/persistence/user/UserRepository.java
+++ src/main/java/com/bamdoliro/sinabro/infrastructure/persistence/user/UserRepository.java
@@ -3,5 +3,9 @@ package com.bamdoliro.sinabro.infrastructure.persistence.user;
 import com.bamdoliro.sinabro.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
