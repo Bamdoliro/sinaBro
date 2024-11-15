9efc8633661883f42e896ac71385dfbd07782d70
cabbage16 <qocn16@gmail.com>
2024-11-15 15:12:24 +0900

    refactor(#1): 구글 계정으로 로그인 HTTP Method 변경 - Google Rest API의 redirect URI를 클라이언트 주소로 변경했어요. - 클라이언트에서 받은 code 값을 다시 서버로 POST로 보내도록 설계를 변경했어요

--- src/main/java/com/bamdoliro/sinabro/presentation/auth/AuthController.java
+++ src/main/java/com/bamdoliro/sinabro/presentation/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.bamdoliro.sinabro.presentation.auth;
 
 import com.bamdoliro.sinabro.application.auth.GoogleAuthLinkUseCase;
 import com.bamdoliro.sinabro.application.auth.GoogleAuthUseCase;
+import com.bamdoliro.sinabro.application.auth.LogOutUseCase;
 import com.bamdoliro.sinabro.application.auth.RefreshAccessTokenUseCase;
 import com.bamdoliro.sinabro.presentation.auth.dto.response.TokenResponse;
 import com.bamdoliro.sinabro.shared.response.CommonResponse;
@@ -18,19 +19,17 @@ public class AuthController {
     private final GoogleAuthLinkUseCase googleAuthLinkUseCase;
     private final GoogleAuthUseCase googleAuthUseCase;
     private final RefreshAccessTokenUseCase refreshAccessTokenUseCase;
+    private final LogOutUseCase logOutUseCase;
 
-    @GetMapping("/google")
+    @GetMapping("/google/link")
     public SingleCommonResponse<String> getGoogleAuthUrl() {
         return CommonResponse.success(
                 googleAuthLinkUseCase.execute()
         );
     }
 
-    // TODO 프론트엔드로 리다이렉트 후 백엔드로 Post 요청 전송
-    // 백엔드에서 직접 GET으로 받으면 보안성이 저하될 수 있고, RESTful하지 않음
-    // 프론트엔드로 리다이렉트 되게 한 후, code를 Request Body에 답아 Post로 전송하는 방식으로 수정해야함
-    @GetMapping("/oauth2/code/google")
-    public SingleCommonResponse<TokenResponse> authGoogle(@RequestParam String code) {
+    @PostMapping("/google")
+    public SingleCommonResponse<TokenResponse> authWithGoogle(@RequestParam String code) {
         return CommonResponse.success(
                 googleAuthUseCase.execute(code)
         );
