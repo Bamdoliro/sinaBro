7520a6ea5b8a120ae9e879434848fa80171e26ee
배재현 <qocn16@gmail.com>
2024-11-16 02:43:08 +0900

    refactor(#1): REST API 설계 원칙 준수 - REST API 설계 원칙에 따라 컬렉션은 복수형으로 표현했어요.

--- src/main/java/com/bamdoliro/sinabro/presentation/user/UserController.java
+++ src/main/java/com/bamdoliro/sinabro/presentation/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
