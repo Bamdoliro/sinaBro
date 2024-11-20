0e144f7b625dc0f0335ae8ee583aeb8ee64835a9
jyj1289 <jin37091289@gmail.com>
2024-11-20 19:29:29 +0900

    refactor(#4): NoArgsConstructor 접근 제어자 변경 - NoArgsConstructor 접근 제어자를 PROTECTED로 변경했어요.

--- src/main/java/com/bamdoliro/sinabro/presentation/character/dto/request/SelectCharacterRequest.java
+++ src/main/java/com/bamdoliro/sinabro/presentation/character/dto/request/SelectCharacterRequest.java
@@ -2,12 +2,13 @@ package com.bamdoliro.sinabro.presentation.character.dto.request;
 
 import com.bamdoliro.sinabro.domain.character.domain.type.CharacterType;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class SelectCharacterRequest {
 
