3a540d9eafde16bbc8c25608e8e59c8fe3ab76d6
jyj1289 <jin37091289@gmail.com>
2024-11-22 00:18:40 +0900

    refactor(#7): 불필요한 예외 삭제 - FCMTokenNotFoundException은 사용하지 않아 제거했어요.

--- src/main/java/com/bamdoliro/sinabro/domain/fcm/token/exception/FcmTokenNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bamdoliro.sinabro.domain.fcm.token.exception;
-
-import com.bamdoliro.sinabro.domain.fcm.token.exception.error.FcmTokenErrorProperty;
-import com.bamdoliro.sinabro.shared.error.SinabroException;
-
-public class FcmTokenNotFoundException extends SinabroException {
-    public FcmTokenNotFoundException() { super(FcmTokenErrorProperty.FCM_TOKEN_NOTFOUND); }
-}
