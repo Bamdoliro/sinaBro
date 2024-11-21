bd318b9c5ecc528c41f0854fbf4c12b4088cff5d
jyj1289 <jin37091289@gmail.com>
2024-11-22 02:52:12 +0900

    feat(#7): 유저별 알림 전체 조회 기능 구현 - 유저별 알림 전체 조회 기능을 구현했어요.

--- src/main/java/com/bamdoliro/sinabro/infrastructure/persistence/notification/NotificationRepository.java
+++ src/main/java/com/bamdoliro/sinabro/infrastructure/persistence/notification/NotificationRepository.java
@@ -1,7 +1,11 @@
 package com.bamdoliro.sinabro.infrastructure.persistence.notification;
 
 import com.bamdoliro.sinabro.domain.notification.domain.Notification;
+import com.bamdoliro.sinabro.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUser(User user);
 }
--- src/main/java/com/bamdoliro/sinabro/presentation/notification/NotificationController.java
+++ src/main/java/com/bamdoliro/sinabro/presentation/notification/NotificationController.java
@@ -1,9 +1,13 @@
 package com.bamdoliro.sinabro.presentation.notification;
 
+import com.bamdoliro.sinabro.application.notification.QueryNotificationListUseCase;
 import com.bamdoliro.sinabro.application.notification.SendNotificationUseCase;
 import com.bamdoliro.sinabro.domain.user.domain.User;
 import com.bamdoliro.sinabro.presentation.notification.dto.request.SendNotificationRequest;
+import com.bamdoliro.sinabro.presentation.notification.dto.response.ListNotificationResponse;
 import com.bamdoliro.sinabro.shared.auth.AuthenticationPrincipal;
+import com.bamdoliro.sinabro.shared.response.CommonResponse;
+import com.bamdoliro.sinabro.shared.response.ListCommonResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +21,7 @@ import java.io.IOException;
 public class NotificationController {
 
     private final SendNotificationUseCase sendNotificationUseCase;
+    private final QueryNotificationListUseCase queryNotificationListUseCase;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping
@@ -25,5 +30,14 @@ public class NotificationController {
             @RequestBody @Valid SendNotificationRequest request
     ) {
         sendNotificationUseCase.execute(user, request);
+    }
+
+    @GetMapping
+    public ListCommonResponse<ListNotificationResponse> queryNotification(
+            @AuthenticationPrincipal User user
+    ) {
+        return CommonResponse.ok(
+            queryNotificationListUseCase.execute(user)
+        );
     }
 }
