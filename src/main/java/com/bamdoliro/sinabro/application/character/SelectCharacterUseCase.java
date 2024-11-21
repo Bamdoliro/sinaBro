9620afd7c2588444994e2f3eba7df44211a63138
jyj1289 <jin37091289@gmail.com>
2024-11-21 17:31:31 +0900

    refactor(#4): 불필요한 어노테이션 삭제 - Transactional 어노테이션을 붙이지 않아도 작동하기 때문에 제거했어요.

--- src/main/java/com/bamdoliro/sinabro/application/character/SelectCharacterUseCase.java
+++ src/main/java/com/bamdoliro/sinabro/application/character/SelectCharacterUseCase.java
@@ -17,7 +17,6 @@ public class SelectCharacterUseCase {
 
     private final CharacterRepository characterRepository;
 
-    @Transactional
     public void execute(User user, SelectCharacterRequest request) {
         validate(user);
 
