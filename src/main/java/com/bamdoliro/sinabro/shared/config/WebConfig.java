8649e37db5f8ace3fac8bd92c8c90a46ccf5cc2e
cabbage16 <qocn16@gmail.com>
2024-11-22 11:52:24 +0900

    feat(#12): Cors Mapping - Cors 설정을 추가했어요.

--- src/main/java/com/bamdoliro/sinabro/shared/config/WebConfig.java
+++ src/main/java/com/bamdoliro/sinabro/shared/config/WebConfig.java
@@ -3,7 +3,9 @@ package com.bamdoliro.sinabro.shared.config;
 import com.bamdoliro.sinabro.shared.auth.AuthenticationArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +19,23 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "https://sinabro.bamdoliro.com"
+                )
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
