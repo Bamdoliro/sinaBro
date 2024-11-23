1a8c2405177d3ebaaffda5e963c41256b1770489
cabbage16 <qocn16@gmail.com>
2024-11-24 08:07:53 +0900

    test(#19): 자주묻는질문 ControllerTest - 자주묻는질문 컨틀롤러 테스트를 작성했어요.

--- src/main/java/com/bamdoliro/sinabro/application/question/UpdateQuestionUseCase.java
+++ src/main/java/com/bamdoliro/sinabro/application/question/UpdateQuestionUseCase.java
@@ -2,7 +2,6 @@ package com.bamdoliro.sinabro.application.question;
 
 import com.bamdoliro.sinabro.domain.question.domain.Question;
 import com.bamdoliro.sinabro.domain.question.service.QuestionFacade;
-import com.bamdoliro.sinabro.infrastructure.persistence.question.QuestionRepository;
 import com.bamdoliro.sinabro.presentation.question.dto.request.QuestionRequest;
 import com.bamdoliro.sinabro.shared.annotation.UseCase;
 import jakarta.transaction.Transactional;
@@ -12,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 @UseCase
 public class UpdateQuestionUseCase {
 
-    private final QuestionRepository questionRepository;
     private final QuestionFacade questionFacade;
 
     @Transactional
--- src/test/java/com/bamdoliro/sinabro/shared/util/ControllerTest.java
+++ src/test/java/com/bamdoliro/sinabro/shared/util/ControllerTest.java
@@ -6,6 +6,7 @@ import com.bamdoliro.sinabro.application.auth.RefreshAccessTokenUseCase;
 import com.bamdoliro.sinabro.application.auth.*;
 import com.bamdoliro.sinabro.application.character.GetCharacterUseCase;
 import com.bamdoliro.sinabro.application.diary.*;
+import com.bamdoliro.sinabro.application.question.*;
 import com.bamdoliro.sinabro.domain.auth.service.TokenService;
 import com.bamdoliro.sinabro.presentation.auth.AuthController;
 import com.bamdoliro.sinabro.presentation.diary.DiaryController;
@@ -21,6 +22,7 @@ import com.bamdoliro.sinabro.presentation.character.CharacterController;
 import com.bamdoliro.sinabro.presentation.fcm.token.FCMTokenController;
 import com.bamdoliro.sinabro.presentation.letter.LetterController;
 import com.bamdoliro.sinabro.presentation.notification.NotificationController;
+import com.bamdoliro.sinabro.presentation.question.QuestionController;
 import com.bamdoliro.sinabro.presentation.user.UserController;
 import com.bamdoliro.sinabro.shared.auth.AuthenticationArgumentResolver;
 import com.bamdoliro.sinabro.shared.auth.AuthenticationExtractor;
@@ -29,6 +31,7 @@ import com.bamdoliro.sinabro.shared.response.SharedController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -42,6 +45,7 @@ import org.springframework.test.web.servlet.MockMvc;
         NotificationController.class,
         FCMTokenController.class,
         CharacterController.class,
+        QuestionController.class,
         LetterController.class,
         SharedController.class
 })
@@ -107,6 +111,22 @@ public abstract class ControllerTest {
     // Character
     @MockBean
     protected SelectCharacterUseCase selectCharacterUseCase;
+
+    // Question
+    @MockBean
+    protected CreateQuestionUseCase createQuestionUseCase;
+
+    @MockBean
+    protected GetAllQuestionUseCase getAllQuestionUseCase;
+
+    @MockBean
+    protected GetQuestionUseCase getQuestionUseCase;
+
+    @MockBean
+    protected UpdateQuestionUseCase updateQuestionUseCase;
+
+    @MockBean
+    protected DeleteQuestionUseCase deleteQuestionUseCase;
 
     @MockBean
     protected GetCharacterUseCase getCharacterUseCase;
