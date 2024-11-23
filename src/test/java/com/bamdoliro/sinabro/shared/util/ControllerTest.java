b7c6df84c65c447d9043d1034b538ebd9190c598
cabbage16 <qocn16@gmail.com>
2024-11-24 08:51:01 +0900

    refactor: 불필요한 import문 제거

--- src/test/java/com/bamdoliro/sinabro/shared/util/ControllerTest.java
+++ src/test/java/com/bamdoliro/sinabro/shared/util/ControllerTest.java
@@ -31,7 +31,6 @@ import com.bamdoliro.sinabro.shared.response.SharedController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
