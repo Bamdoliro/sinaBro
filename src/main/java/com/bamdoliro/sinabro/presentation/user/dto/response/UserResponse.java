5fdf627b9f37046e5e069b0de354c5036d711d39
cabbage16 <qocn16@gmail.com>
2024-11-25 10:18:39 +0900

    fix: UserResponse authority 추가 - 유저 정보를 가져올 때 유저의 권한이 나타나도록 변경했어요.

--- src/main/java/com/bamdoliro/sinabro/presentation/user/dto/response/UserResponse.java
+++ src/main/java/com/bamdoliro/sinabro/presentation/user/dto/response/UserResponse.java
@@ -1,6 +1,7 @@
 package com.bamdoliro.sinabro.presentation.user.dto.response;
 
 import com.bamdoliro.sinabro.domain.user.domain.User;
+import com.bamdoliro.sinabro.domain.user.domain.type.Authority;
 import lombok.Getter;
 
 @Getter
@@ -8,9 +9,11 @@ public class UserResponse {
 
     private final String email;
     private final String name;
+    private final Authority authority;
 
     public UserResponse(User user) {
         this.email = user.getEmail();
         this.name = user.getName();
+        this.authority = user.getAuthority();
     }
 }
